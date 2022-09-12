e3a925bb31b2d476a6e6a950e40fc36295c70096
Rafaela Breves <rafaela.curty13@gmail.com>
2022-09-12 15:00:03 -0300

    pontos turisticos e descricao

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const app = {
+    data() {
+      return {
+        pontosTuristicos: [
+          { title: '<p style="color: white;">Cristo Redentor</p>', img: '../assets/images/cristo.jpeg', imgAlt: 'vista do cristo redentor' },
+          { title: '<p style="color: white;">Pão de Açúcar</p>', img: '../assets/images/cristo.jpeg', imgAlt: 'vista do cristo redentor' },
+          { title: '<p style="color: white;">Arcos da Lapa</p>', img: '../assets/images/cristo.jpeg', imgAlt: 'vista do cristo redentor' },
+          { title: '<p style="color: white;">Bondinho</p>', img: '../assets/images/cristo.jpeg', imgAlt: 'vista do cristo redentor' },
+        ]
+      };
+    }
+  };
+  
+  Vue.createApp(app).mount('#app');
