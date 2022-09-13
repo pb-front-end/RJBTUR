bc4d9ffd10bd1db8fdb32b6d4b253caf265ca193
kick250 <kicklobal@gmail.com>
2022-09-12 22:19:04 -0300

    adicionar eventos

--- javascript/index.js
+++ javascript/index.js
@@ -6,6 +6,7 @@ const app = {
         { title: '<p style="color: white;">Hospedagens</p>', img: '/assets/images/copacabana.jpg', imgAlt: 'vista de copacabana' },
         { title: '<p style="color: white;">Restaurantes</p>', img: '/assets/images/copacabana.jpg', imgAlt: 'vista de copacabana' },
         { title: '<p style="color: white;">Hospitais</p>', img: '/assets/images/copacabana.jpg', imgAlt: 'vista de copacabana' },
+        { title: '<p style="color: white;">Eventos</p>', img: '/assets/images/copacabana.jpg', imgAlt: 'vista de copacabana' },
       ]
     };
   }
