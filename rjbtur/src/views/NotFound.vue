a066bedd24de8c05e3678780d6459ee31582bd85
Rafaela Breves <rafaela.curty13@gmail.com>
2022-09-22 23:11:14 -0300

    add notfound route

---
+++ rjbtur/src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="content">
+        <header-component/>
+
+        <h1>Rota não encontrada <img src="../assets/icons/face-sad-tear-solid.svg" class="sad"> </h1>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue'
+
+export default {
+    components: { HeaderComponent }
+}
+</script>
+
+<style scoped>
+.content {
+    text-align: center;
+}
+
+.sad {
+    max-height: 40px;
+}
+</style>
